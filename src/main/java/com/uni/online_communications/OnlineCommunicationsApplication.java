b632534cbaf66d68fc820a17072e4c967fbeb0fa
Krastyo Tanev <kr.tanev01@gmail.com>
2025-01-04 11:32:21 +0200

    init

---
+++ src/main/java/com/uni/online_communications/OnlineCommunicationsApplication.java
@@ -0,0 +1,16 @@
+package com.uni.online_communications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCommunicationsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting...");
+
+		SpringApplication.run(OnlineCommunicationsApplication.class, args);
+
+	}
+
+}
